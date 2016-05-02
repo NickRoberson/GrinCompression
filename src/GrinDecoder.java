25623ad48807ec645ede98f5e54bf0a7b2e033ad
Nick Roberson <roberson@grinnell.edu>
2016-05-01 20:59:26 -0500

    added encoder and decoder framework

--- src/GrinDecoder.java
+++ src/GrinDecoder.java
@@ -1,4 +1,30 @@
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GrinDecoder {
 
+	public void decode (String infile, String outfile) throws IOException {
+		
+		// create input and output
+		BitInputStream input = new BitInputStream(infile);
+		BitOutputStream output = new BitOutputStream(outfile);
+		
+		// parse header
+		int magicNumber = input.readBits(32);
+		int codes = input.readBits(32);
+		
+		// construct frequency map from header
+		Map<Short, Integer> freqMap = new HashMap<>();
+		Short codeVal;
+		Integer occurrences;
+		for (int i = 0; i < codes; i++) {
+			codeVal = (short) input.readBits(16);
+			occurrences = input.readBits(32);
+			freqMap.put(codeVal, occurrences);
+		}
+		
+		// construct a huffman tree from frequency map
+		HuffmanTree hTree = new HuffmanTree(freqMap);
+	}
 }
